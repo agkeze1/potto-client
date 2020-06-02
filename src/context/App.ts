855a8217261a1cdc06b44eed0e6c54332bcbc140
Otis Chiemezie <otisidevelop@gmail.com>
2020-06-02 15:33:52 +0100

    Added settings that would alow the app to render different style based on host
    Added

--- src/context/App.ts
+++ src/context/App.ts
@@ -1,14 +1,20 @@
-export const GetAppName = (): string => "SpacoSpy";
+const { REACT_APP_ID: key, REACT_APP_LOGO, REACT_APP_NAME, REACT_APP_FULL_NAME } = process.env;
+
+export const GetAppName = (): string => REACT_APP_NAME || "";
 
 export const GET_API_URL = () => process.env.REACT_APP_BASEURL;
+export const GET_APP_ID = () => key || "potto";
+
+export const GET_LOGO = REACT_APP_LOGO;
+export const GET_FULL_NAME = REACT_APP_FULL_NAME;
 
 export const CLEAN_DATE = (date: string): string => {
-  if (date)
-    return Intl.DateTimeFormat("en-US", {
-      month: "short",
-      year: "numeric",
-      day: "2-digit",
-      weekday: "short"
-    }).format(new Date(date));
-  return "";
+    if (date)
+        return Intl.DateTimeFormat("en-US", {
+            month: "short",
+            year: "numeric",
+            day: "2-digit",
+            weekday: "short",
+        }).format(new Date(date));
+    return "";
 };

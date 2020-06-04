fcff9f77e783b711e99d20b3aca6caeabc637c73
Otis Chiemezie <otisidevelop@gmail.com>
2020-06-04 15:07:06 +0100

    Update IconInput.tsx

--- src/pages/partials/IconInput.tsx
+++ src/pages/partials/IconInput.tsx
@@ -1,51 +1,43 @@
 import React, { FC } from "react";
 
 interface props {
-  icon: string;
-  placeholder: string;
-  label: string;
-  required: boolean;
-  type: string;
-  onChange?: any;
-  classStyle?: string;
-  initVal?: string;
-  disabled?: boolean;
+    icon: string;
+    placeholder: string;
+    label: string;
+    required: boolean;
+    type: string;
+    onChange?: any;
+    classStyle?: string;
+    initVal?: string;
+    disabled?: boolean;
+    name?: string;
 }
 
-const IconInput: FC<props> = ({
-  icon,
-  placeholder,
-  label,
-  required,
-  type,
-  onChange,
-  classStyle,
-  initVal,
-  disabled,
-}) => {
-  return (
-    <>
-      <label htmlFor="email">{label}</label>
-      <div className="input-group mb-3">
-        <div className="input-group-prepend">
-          <div className="input-group-text">
-            <div className={`os-icon ${icon}`}></div>
-          </div>
-        </div>
-        <input
-          className={`form-control ${classStyle}`}
-          placeholder={placeholder}
-          required={required}
-          value={initVal}
-          disabled={disabled}
-          onChange={({ target }) => {
-            if (onChange) onChange(target.value);
-          }}
-          type={type}
-        />
-      </div>
-    </>
-  );
+const IconInput: FC<props> = ({ icon, placeholder, label, required, type, onChange, classStyle, initVal, disabled, name }) => {
+    return (
+        <>
+            <label htmlFor="email">{label}</label>
+            <div className="input-group mb-3">
+                <div className="input-group-prepend">
+                    <div className="input-group-text">
+                        <div className={`os-icon ${icon}`}></div>
+                    </div>
+                </div>
+                <input
+                    className={`form-control ${classStyle}`}
+                    placeholder={placeholder}
+                    required={required}
+                    value={initVal}
+                    disabled={disabled}
+                    name={name}
+                    onChange={({ target }) => {
+                        if (onChange) onChange(target.value);
+                    }}
+                    type={type}
+                />
+            </div>
+        </>
+    );
 };
 
 export default IconInput;

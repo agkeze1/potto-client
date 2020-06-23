0a16fef7182c21765e8e591e4b25af822984d143
Otis Chiemezie <otisidevelop@gmail.com>
2020-06-23 10:53:00 +0100

    Create teacher.model.ts

---
+++ src/models/teacher.model.ts
@@ -0,0 +1,12 @@
+export class Teacher {
+    id = "";
+    name = "";
+    email = "";
+    phone = "";
+    first_name = "";
+    gender = "";
+    image = "";
+    dob = "";
+    doj = "";
+    address = "";
+}

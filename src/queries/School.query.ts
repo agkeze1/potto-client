0e49d5da540bb48f4f8bd02a0afaf1863ca26ae5
Otis Chiemezie <otisidevelop@gmail.com>
2020-09-20 07:17:34 +0100

    Updated school query

--- src/queries/School.query.ts
+++ src/queries/School.query.ts
@@ -2,90 +2,90 @@ import gql from "graphql-tag";
 import { USER_PROPS } from "./User.query";
 
 export const SCHOOL_PROPS = gql`
-  fragment SCHOOL_PROPS on School {
-    id
-    name
-    alias
-    logo
-    contact_address
-    contact_email
-    contact_phone
-    address
-    created_at
-    primary_color
-    secondary_color
-  }
+    fragment SCHOOL_PROPS on School {
+        id
+        name
+        alias
+        logo
+        contact_address
+        contact_email
+        contact_phone
+        address
+        created_at
+        primary_color
+        secondary_color
+    }
 `;
 
 // Query Section
 export const HAS_SCHOOL = gql`
-  query HAS_SCHOOL {
-    HasSchool
-  }
+    {
+        HasSchool
+    }
 `;
 
 export const GET_SCHOOL_LIST = gql`
-  query GET_SCHOOL_LIST($page: Int, $limit: Int) {
-    GetSchools(page: $page, limit: $limit) {
-      docs {
-        ...SCHOOL_PROPS
-      }
-      totalDocs
-      totalPages
-      page
-      limit
-      nextPage
-      prevPage
+    query GET_SCHOOL_LIST($page: Int, $limit: Int) {
+        GetSchools(page: $page, limit: $limit) {
+            docs {
+                ...SCHOOL_PROPS
+            }
+            totalDocs
+            totalPages
+            page
+            limit
+            nextPage
+            prevPage
+        }
     }
-  }
-  ${SCHOOL_PROPS}
+    ${SCHOOL_PROPS}
 `;
 
 export const GET_SCHOOL = gql`
-  query GET_SCHOOL($id: ID!) {
-    GetSchool(id: $id) {
-      doc {
-        ...SCHOOL_PROPS
-      }
+    query GET_SCHOOL($id: ID!) {
+        GetSchool(id: $id) {
+            doc {
+                ...SCHOOL_PROPS
+            }
+        }
     }
-  }
-  ${SCHOOL_PROPS}
+    ${SCHOOL_PROPS}
 `;
 
 // Mutation Section
 export const NEW_SCHOOL = gql`
-  mutation NEW_SCHOOL($model: SchoolInput!) {
-    NewSchool(model: $model) {
-      message
-      doc {
-        ...SCHOOL_PROPS
-      }
+    mutation NEW_SCHOOL($model: SchoolInput!) {
+        NewSchool(model: $model) {
+            message
+            doc {
+                ...SCHOOL_PROPS
+            }
+        }
     }
-  }
-  ${SCHOOL_PROPS}
+    ${SCHOOL_PROPS}
 `;
 
 export const MAKE_PRIMARY = gql`
-  mutation MAKE_PRIMARY($school: ID!) {
-    MakePrimarySchool(school: $school) {
-      message
-      token
-      doc {
-        ...USER_PROPS
-      }
+    mutation MAKE_PRIMARY($school: ID!) {
+        MakePrimarySchool(school: $school) {
+            message
+            token
+            doc {
+                ...USER_PROPS
+            }
+        }
     }
-  }
-  ${USER_PROPS}
+    ${USER_PROPS}
 `;
 
 export const UPDATE_SCHOOL = gql`
-  mutation UPDATE_SCHOOL($id: ID, $update: SchoolInput!) {
-    UpdateSchool(id: $id, update: $update) {
-      message
-      doc {
-        ...SCHOOL_PROPS
-      }
+    mutation UPDATE_SCHOOL($id: ID, $update: SchoolInput!) {
+        UpdateSchool(id: $id, update: $update) {
+            message
+            doc {
+                ...SCHOOL_PROPS
+            }
+        }
     }
-  }
-  ${SCHOOL_PROPS}
+    ${SCHOOL_PROPS}
 `;
